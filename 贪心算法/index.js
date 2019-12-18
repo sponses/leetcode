b3de2053defbb4e575a01052271c93a4abd9e250
滕怡君 <1057120672@qq.com>
2019-12-18 14:47:43 +0800

    122.买卖股票的最佳时机Ⅱ

--- 贪心算法/index.js
+++ 贪心算法/index.js
@@ -18,3 +18,17 @@ var findContentChildren = function(g, s) {
   }
   return child
 }
+/**
+ * 122.买卖股票的最佳时机Ⅱ
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  let maxProfit = 0
+  for (let i = 0, len = prices.length - 1; i < len; i++) {
+    if (prices[i] < prices[i + 1]) {
+      maxProfit += prices[i + 1] - prices[i]
+    }
+  }
+  return maxProfit
+}
