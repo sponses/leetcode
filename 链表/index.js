c175c3c5cec96bca025c6a64fdf66c19ed8f1923
滕怡君 <1057120672@qq.com>
2019-12-19 17:02:21 +0800

    206.反转单链表

---
+++ 链表/index.js
@@ -0,0 +1,22 @@
+/**
+ * 206.反转链表
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  let prev = null
+  while (head) {
+    let temp = head.next
+    head.next = prev
+    prev = head
+    head = temp
+  }
+  return head
+}
