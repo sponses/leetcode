0595eb542ebbdb8b278e42d2ca0864cc248fa15c
滕怡君 <1057120672@qq.com>
2019-12-20 09:22:30 +0800

    914.卡牌分组

--- 数组/index.js
+++ 数组/index.js
@@ -18,3 +18,34 @@ var findUnsortedSubarray = function(nums) {
   }
   return count === nums.length ? 0 : end - start + 1
 }
+/**
+ * 914.卡牌分组
+ * @param {number[]} deck
+ * @return {boolean}
+ */
+var hasGroupsSizeX = function(deck) {
+  if (!deck.length) return false
+  function gcd(a, b) {
+    if (a % b === 0) return b
+    return gcd(b, a % b)
+  }
+  deck.sort((a, b) => a - b)
+  let arr = [],
+    count = 0
+  for (let i = 0, len = deck.length; i < len; i++) {
+    if (deck[i] === deck[i + 1]) {
+      count++
+    } else {
+      count++
+      arr.push(count)
+      count = 0
+    }
+  }
+  if (Math.min(...arr) === 1) return false
+  for (let i = 0, len = arr.length - 1; i < len; i++) {
+    if (gcd(arr[i], arr[i + 1]) <= 1) {
+      return false
+    }
+  }
+  return true
+}
