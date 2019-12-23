a628d0254a8a59099e8cfa9cdb148a0c101d919b
滕怡君 <1057120672@qq.com>
2019-12-23 11:50:57 +0800

    125.验证回文串（双指针）

--- 栈/index.js
+++ 栈/index.js
@@ -1,5 +1,5 @@
 /**
- * 125.验证回文串
+ * 125.验证回文串（栈）
  * @param {string} s
  * @return {boolean}
  */
@@ -18,6 +18,26 @@ var isPalindrome = function(s) {
     if (arr[arr.length - 1].toLocaleLowerCase() !== s[i].toLocaleLowerCase())
       return false
     arr.pop()
+  }
+  return true
+}
+/**
+ * 125.验证回文串（双指针）
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+  s = s.replace(
+    /[\ |\~|\`|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\-|\_|\+|\=|\||\\|\[|\]|\{|\}|\;|\:|\"|\'|\,|\<|\.|\>|\/|\?]/g,
+    ''
+  )
+  for (let i = 0, len = s.length; i < len; i++) {
+    if (i <= len - i - 1) {
+      if (s[i].toLocaleLowerCase() !== s[len - i - 1].toLocaleLowerCase())
+        return false
+    } else {
+      break
+    }
   }
   return true
 }
