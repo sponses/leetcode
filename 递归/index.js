6b923231fae04dcb16210d28696e82fe45516f56
滕怡君 <1057120672@qq.com>
2019-12-30 14:56:22 +0800

    783.二叉搜索树节点最小距离（中序遍历）

--- 递归/index.js
+++ 递归/index.js
@@ -1,5 +1,5 @@
 /**
- * 783.二叉搜索树节点最小距离
+ * 783.二叉搜索树节点最小距离（数组排序）
  * Definition for a binary tree node.
  * function TreeNode(val) {
  *     this.val = val;
@@ -27,5 +27,31 @@ var minDiffInBST = function(root) {
   for (let i = 0, len = arr.length - 1; i < len; i++) {
     min = Math.min(min, arr[i + 1] - arr[i])
   }
+  return min
+}
+/**
+ * 783.二叉搜索树节点最小距离（中序遍历）
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDiffInBST = function(root) {
+  let prev = null,
+    min = Number.MAX_SAFE_INTEGER
+
+  function dfs(root) {
+    if (!root) return
+    dfs(root.left)
+    if (prev !== null) min = Math.min(min, root.val - prev)
+    prev = root.val
+    dfs(root.right)
+  }
+  dfs(root)
   return min
 }
