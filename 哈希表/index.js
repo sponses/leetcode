3a68a9b2b072a976880b565c3c754b30699c0eaa
滕怡君 <1057120672@qq.com>
2019-12-31 09:26:39 +0800

    219. 存在重复元素Ⅱ

--- 哈希表/index.js
+++ 哈希表/index.js
@@ -13,3 +13,19 @@ var containsDuplicate = function(nums) {
   }
   return false
 }
+/**
+ * 219. 存在重复元素Ⅱ
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+  let obj = {}
+  for (let i = 0, len = nums.length; i < len; i++) {
+    if (obj.hasOwnProperty(nums[i])) {
+      if (Math.abs(i - obj[nums[i]]) <= k) return true
+    }
+    obj[nums[i]] = i
+  }
+  return false
+}
