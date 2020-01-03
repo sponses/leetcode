6429d39ba9970486740c39a76e81f1c57d8fd7e9
滕怡君 <1057120672@qq.com>
2020-01-03 10:34:48 +0800

    21. 合并两个有序链表

--- 链表/index.js
+++ 链表/index.js
@@ -191,3 +191,33 @@ var oddEvenList = function(head) {
   odd.next = list
   return head
 }
+/**
+ * 21. 合并两个有序链表
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+  let result = new ListNode(null),
+    dummy = result
+
+  while (l1 || l2) {
+    if ((l1 && l2 && l1.val <= l2.val) || (l1 && !l2)) {
+      result.next = new ListNode(l1.val)
+      result = result.next
+      l1 = l1.next
+    } else if ((l1 && l2 && l2.val < l1.val) || (!l1 && l2)) {
+      result.next = new ListNode(l2.val)
+      result = result.next
+      l2 = l2.next
+    }
+  }
+  return dummy.next
+}
