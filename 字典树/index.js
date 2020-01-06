87ad9b95169f61b8d1b22e3c24256f06ea4dc05a
滕怡君 <1057120672@qq.com>
2020-01-06 13:38:30 +0800

    211. 添加与搜索单词 - 数据结构设计

--- 字典树/index.js
+++ 字典树/index.js
@@ -53,3 +53,68 @@ Trie.prototype.startsWith = function(prefix) {
   }
   return startsWith(prefix, this.root)
 }
+/**
+ * 211. 添加与搜索单词 - 数据结构设计
+ * Initialize your data structure here.
+ */
+var WordDictionary = function() {
+  this.root = {}
+}
+
+/**
+ * Adds a word into the data structure.
+ * @param {string} word
+ * @return {void}
+ */
+WordDictionary.prototype.addWord = function(word) {
+  function insert(word, root) {
+    if (!word.length) return
+
+    let letter = word[0]
+    if (!root.hasOwnProperty(letter))
+      root[letter] = { isEnd: word.length === 1 }
+    if (word.length === 1) {
+      root[letter].isEnd = true
+      return
+    }
+    return insert(word.slice(1), root[letter])
+  }
+  insert(word, this.root)
+}
+
+/**
+ * Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+ * @param {string} word
+ * @return {boolean}
+ */
+WordDictionary.prototype.search = function(word) {
+  function search(word, root) {
+    if (!word.length) return false
+
+    let letter = word[0]
+    if (letter === '.') {
+      let arr = Object.keys(root).filter(x => !x !== 'isEnd')
+      let result = false
+      for (let i = 0, len = arr.length; i < len; i++) {
+        if (word.length === 1) {
+          if (root[arr[i]].isEnd) return true
+        } else {
+          result = search(word.slice(1), root[arr[i]])
+          if (result) break
+        }
+      }
+      return result
+    }
+    if (!root.hasOwnProperty(letter)) return false
+    if (word.length === 1 && root[letter].isEnd) return true
+    return search(word.slice(1), root[letter])
+  }
+  return search(word, this.root)
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * var obj = new WordDictionary()
+ * obj.addWord(word)
+ * var param_2 = obj.search(word)
+ */
