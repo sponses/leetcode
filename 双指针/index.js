35b6a240b66e6b78dc93c5bc49ba174b143e3fc5
滕怡君 <1057120672@qq.com>
2020-01-06 09:32:14 +0800

    977. 有序数组的平方

--- 双指针/index.js
+++ 双指针/index.js
@@ -10,3 +10,37 @@ var reverseString = function(s) {
     if (i >= Math.floor(len / 2) - 1) return
   }
 }
+/**
+ * 977. 有序数组的平方
+ * @param {number[]} A
+ * @return {number[]}
+ */
+var sortedSquares = function(A) {
+  let j = 0,
+    len = A.length
+  while (A[j] < 0) {
+    j++
+  }
+  let i = j - 1,
+    result = []
+
+  while (i >= 0 && j < len) {
+    if (Math.pow(A[i], 2) > Math.pow(A[j], 2)) {
+      result.push(Math.pow(A[j], 2))
+      j++
+    } else {
+      result.push(Math.pow(A[i], 2))
+      i--
+    }
+  }
+
+  while (i >= 0) {
+    result.push(Math.pow(A[i], 2))
+    i--
+  }
+  while (j < len) {
+    result.push(Math.pow(A[j], 2))
+    j++
+  }
+  return result
+}
