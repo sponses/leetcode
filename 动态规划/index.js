9ea6d382b142b9abf3d9ad375f493d33361f3bb4
滕怡君 <1057120672@qq.com>
2020-01-07 11:11:03 +0800

    322. 零钱兑换（备忘录递归通过）

--- 动态规划/index.js
+++ 动态规划/index.js
@@ -17,3 +17,31 @@ var coinChange = function(coins, amount) {
   }
   return min === Number.MAX_SAFE_INTEGER ? -1 : min
 }
+
+/**
+ * 322. 零钱兑换（备忘录递归通过）
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+  let obj = {},
+    len = coins.length
+
+  function change(amount) {
+    if (amount === 0) return 0
+
+    let min = Number.MAX_SAFE_INTEGER
+    for (let i = 0; i < len; i++) {
+      if (coins[i] > amount) continue
+
+      if (!obj.hasOwnProperty(amount - coins[i])) {
+        obj[amount - coins[i]] = change(amount - coins[i])
+      }
+      if (obj[amount - coins[i]] === -1) continue
+      min = Math.min(min, 1 + obj[amount - coins[i]])
+    }
+    return min === Number.MAX_SAFE_INTEGER ? -1 : min
+  }
+  return change(amount)
+}
