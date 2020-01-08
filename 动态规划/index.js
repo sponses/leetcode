16c1f3fbfa05f1e02a0dc388306f13e649d36eb2
滕怡君 <1057120672@qq.com>
2020-01-08 14:04:46 +0800

    70. 爬楼梯（动态规划）

--- 动态规划/index.js
+++ 动态规划/index.js
@@ -126,3 +126,20 @@ var rob = function(nums) {
 
   return max
 }
+/**
+ * 70. 爬楼梯（动态规划）
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+  if (n === 1) return 1
+
+  let a = 1,
+    b = 2
+  for (let i = 3; i <= n; i++) {
+    let temp = a + b
+    a = b
+    b = temp
+  }
+  return b
+}
