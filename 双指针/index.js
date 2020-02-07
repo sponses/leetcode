806251078a9aa5d4c0c4e838fa40fa131b784bc5
滕怡君 <1057120672@qq.com>
2020-02-07 15:01:00 +0800

    26. 删除排序数组中的重复项

--- 双指针/index.js
+++ 双指针/index.js
@@ -79,3 +79,19 @@ var arrayPairSum = function(nums) {
   }
   return res
 }
+/**
+ * 26. 删除排序数组中的重复项
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  if (!nums.length) return 0
+  let i = 0
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[i] !== nums[j]) {
+      i++
+      nums[i] = nums[j]
+    }
+  }
+  return i + 1
+}
