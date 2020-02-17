ecdbbe67cdca1d7e2f5252ff172684c80f0b5411
滕怡君 <1057120672@qq.com>
2020-02-17 16:44:22 +0800

    448. 找到所有数组中消失的数字（桶排序）

--- 桶排序/index.js
+++ 桶排序/index.js
@@ -16,3 +16,22 @@ var firstMissingPositive = function(nums) {
   }
   return nums.length + 1
 }
+/**
+ * 448. 找到所有数组中消失的数字（桶排序）
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+  let res = []
+  for (let i = 0, len = nums.length; i < len; i++) {
+    let cur = nums[i]
+    while (cur !== i + 1 && nums[cur - 1] !== cur) {
+      ;[nums[cur - 1], nums[i]] = [nums[i], nums[cur - 1]]
+      cur = nums[i]
+    }
+  }
+  for (let i = 0, len = nums.length; i < len; i++) {
+    if (nums[i] !== i + 1) res.push(i + 1)
+  }
+  return res
+}
