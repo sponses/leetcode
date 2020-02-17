086ec18fd2e55554c084766cad24a5530e010a1a
滕怡君 <1057120672@qq.com>
2020-02-17 17:50:49 +0800

    160. 相交链表

--- 链表/index.js
+++ 链表/index.js
@@ -555,3 +555,19 @@ var reverseKGroup = function(head, k) {
   }
   return dummy.next
 }
+/**
+ * 160. 相交链表
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+  if (!headA || !headB) return null
+  let pA = headA,
+    pB = headB
+  while (pA != pB) {
+    pA = pA == null ? headB : pA.next
+    pB = pB == null ? headA : pB.next
+  }
+  return pA
+}
