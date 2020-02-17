a1bd71492cf4fa5ee88efb125c673bcbc89d7b0a
滕怡君 <1057120672@qq.com>
2020-02-17 09:12:05 +0800

    142. 环形链表 II（快慢指针）

--- 快慢指针/index.js
+++ 快慢指针/index.js
@@ -14,3 +14,25 @@ var hasCycle = function(head) {
   }
   return false
 }
+/**
+ * 142. 环形链表 II（快慢指针）
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+  if (!head) return null
+  let slow = head,
+    fast = head
+  while (slow) {
+    fast = fast.next ? fast.next.next : fast.next
+    if (!fast) return null
+    slow = slow.next
+    if (fast === slow) break
+  }
+  slow = head
+  while (slow !== fast) {
+    slow = slow.next
+    fast = fast.next
+  }
+  return fast
+}
