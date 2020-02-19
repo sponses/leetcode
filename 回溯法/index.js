702653da23c14c89bc8bce1bb895328cec0166e7
滕怡君 <1057120672@qq.com>
2020-02-19 14:55:10 +0800

    78. 子集（枚举）

--- 回溯法/index.js
+++ 回溯法/index.js
@@ -1,28 +1,18 @@
-from typing import List
-
-
-class Solution:
-    def letterCasePermutation(self, S: str) -> List[str]:
-        size = len(S)
-
-        if size == 0:
-            return []
-
-        res = []
-        path = []
-        self.__dfs(S, size, 0, path, res)
-        return res
-
-    def __dfs(self, S, size, index, path, res):
-        if index == size:
-            return res.append(''.join(path))
-
-        path.append(S[index])
-        self.__dfs(S, size, index + 1, path, res)
-        path.pop()
-
-        # 如果是字母，就变换大小写
-        if S[index].isalpha():
-            path.append(chr(ord(S[index]) ^ (1 << 5)))
-            self.__dfs(S, size, index + 1, path, res)
-            path.pop()
+/**
+ * 78. 子集（枚举）
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+  let res = [[]]
+  for (let i = 0, len = nums.length; i < len; i++) {
+    let count = res.length
+    while (count) {
+      let temp = [...res[count - 1]]
+      temp.push(nums[i])
+      res.push(temp)
+      count--
+    }
+  }
+  return res
+}
