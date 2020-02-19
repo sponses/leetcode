d1e1c1f97b73f2730737f9b00c41ea90d962cd3d
滕怡君 <1057120672@qq.com>
2020-02-19 15:31:43 +0800

    17. 电话号码的字母组合（枚举）

--- 回溯法/index.js
+++ 回溯法/index.js
@@ -16,3 +16,35 @@ var subsets = function(nums) {
   }
   return res
 }
+/**
+ * 17. 电话号码的字母组合（枚举）
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+  let hash = {
+      2: 'abc',
+      3: 'def',
+      4: 'ghi',
+      5: 'jkl',
+      6: 'mno',
+      7: 'pqrs',
+      8: 'tuv',
+      9: 'wxyz'
+    },
+    res = []
+  if (digits <= 1) return res
+  res = [...hash[digits[0]]]
+  for (let i = 1, len1 = digits.length; i < len1; i++) {
+    let count = res.length,
+      len = count
+    while (count) {
+      for (let k = 0, len3 = hash[digits[i]].length; k < len3; k++) {
+        res.push(res[count - 1] + hash[digits[i]][k])
+      }
+      count--
+    }
+    res.splice(0, len)
+  }
+  return res
+}
