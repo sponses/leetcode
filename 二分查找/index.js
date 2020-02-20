f44369c40c567f2a1b34b4567a5207ae8fe356e1
滕怡君 <1057120672@qq.com>
2020-02-20 16:42:54 +0800

    69. x 的平方根（二分法）

--- 二分查找/index.js
+++ 二分查找/index.js
@@ -45,3 +45,22 @@ var searchRange = function(nums, target) {
   }
   return [-1, -1]
 }
+/**
+ * 69. x 的平方根（二分法）
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+  let left = 1,
+    right = x
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2)
+    if (mid * mid <= x && (mid + 1) * (mid + 1) > x) return mid
+    if (mid * mid > x) {
+      right = mid - 1
+    } else {
+      left = mid + 1
+    }
+  }
+  return 0
+}
