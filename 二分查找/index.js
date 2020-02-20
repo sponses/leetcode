cd1b66d385b3403ca18b312286d141755e06ef27
滕怡君 <1057120672@qq.com>
2020-02-20 15:17:14 +0800

    34. 在排序数组中查找元素的第一个和最后一个位置（二分查找）

--- 二分查找/index.js
+++ 二分查找/index.js
@@ -19,3 +19,29 @@ var search = function(nums, target) {
   }
   return -1
 }
+/**
+ * 34. 在排序数组中查找元素的第一个和最后一个位置（二分查找）
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+  let left = 0,
+    right = nums.length - 1
+  while (left <= right) {
+    if (left === right) return nums[left] === target ? [left, left] : [-1, -1]
+    let mid = left + Math.floor((right - left) / 2)
+    if (nums[mid] < target) {
+      left = mid + 1
+    } else if (nums[mid] === target) {
+      let temp1 = mid,
+        temp2 = mid
+      while (nums[temp1 - 1] === target) temp1--
+      while (nums[temp2 + 1] === target) temp2++
+      return [temp1, temp2]
+    } else {
+      right = mid - 1
+    }
+  }
+  return [-1, -1]
+}
