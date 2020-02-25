3ead0adf155e6456e3aca69e1c0ad2becba1ff31
滕怡君 <1057120672@qq.com>
2020-02-25 15:47:13 +0800

    72. 编辑距离（暴力递归）

--- 动态规划/index.js
+++ 动态规划/index.js
@@ -448,3 +448,20 @@ var lengthOfLIS = function(nums) {
   }
   return res
 }
+/**
+ * 72. 编辑距离（暴力递归）
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function(word1, word2) {
+  function dp(i, j) {
+    if (i === -1 && j === -1) return 0
+    if (i === -1) return j + 1
+    if (j === -1) return i + 1
+    if (word1[i] === word2[j]) return dp(i - 1, j - 1)
+    let res = 1 + Math.min(dp(i, j - 1), dp(i - 1, j), dp(i - 1, j - 1))
+    return res
+  }
+  return dp(word1.length - 1, word2.length - 1)
+}
