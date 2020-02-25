36b9d0ff4173e00de5d5cc8b244c10aaaf54e0b0
滕怡君 <1057120672@qq.com>
2020-02-25 17:02:28 +0800

    72. 编辑距离（动态规划）

--- 动态规划/index.js
+++ 动态规划/index.js
@@ -465,3 +465,31 @@ var minDistance = function(word1, word2) {
   }
   return dp(word1.length - 1, word2.length - 1)
 }
+/**
+ * 72. 编辑距离（动态规划）
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function(word1, word2) {
+  let len1 = word1.length,
+    len2 = word2.length
+  let dp = new Array(len1 + 1)
+  for (let i = 0; i <= len1; i++) {
+    dp[i] = new Array(len2)
+    dp[i][0] = i
+  }
+  for (let j = 0; j <= len2; j++) {
+    dp[0][j] = j
+  }
+  for (let i = 1; i <= len1; i++) {
+    for (let j = 1; j <= len2; j++) {
+      if (word2[j - 1] === word1[i - 1]) {
+        dp[i][j] = dp[i - 1][j - 1]
+      } else {
+        dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j])
+      }
+    }
+  }
+  return dp[len1][len2]
+}
