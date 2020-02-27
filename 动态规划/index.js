a4e9495082e1b99e89c9f36a14e8099cac7fc600
滕怡君 <1057120672@qq.com>
2020-02-27 15:01:17 +0800

    198. 打家劫舍（动态规划）

--- 动态规划/index.js
+++ 动态规划/index.js
@@ -665,3 +665,17 @@ var rob = function(nums) {
   }
   return dp(nums.length - 1)
 }
+/**
+ * 198. 打家劫舍（动态规划）
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+  let len = nums.length,
+    dp = new Array(len + 1)
+  ;(dp[0] = 0), (dp[1] = nums[0])
+  for (let i = 2; i <= len; i++) {
+    dp[i] = Math.max(nums[i - 1] + dp[i - 2], dp[i - 1])
+  }
+  return dp[len]
+}
