36395a25188eea8174dbeb5d4f071cbb33a8a801
滕怡君 <1057120672@qq.com>
2020-02-27 11:34:42 +0800

    122. 买卖股票的最佳时机 II（动态规划）

--- 动态规划/index.js
+++ 动态规划/index.js
@@ -629,3 +629,26 @@ var maxProfit = function(prices) {
   }
   return dp[len - 1][0]
 }
+/**
+ * 122. 买卖股票的最佳时机 II（动态规划）
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  if (prices.length === 0) return 0
+  let len = prices.length,
+    dp = new Array(len)
+  for (let i = 0; i < len; i++) {
+    dp[i] = new Array(2)
+  }
+  for (let i = 0; i < len; i++) {
+    if (i === 0) {
+      dp[i][0] = 0
+      dp[i][1] = -prices[i]
+    } else {
+      dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+      dp[i][1] = Math.max(dp[i - 1][0] - prices[i], dp[i - 1][1])
+    }
+  }
+  return dp[len - 1][0]
+}
