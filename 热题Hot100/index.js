ba30cbbdaa92b940013170c9ccd02d7e344c8860
滕怡君 <1057120672@qq.com>
2020-03-05 11:51:43 +0800

    617. 合并二叉树

--- 热题Hot100/index.js
+++ 热题Hot100/index.js
@@ -1181,3 +1181,16 @@ var maxProduct = function(nums) {
   }
   return res
 }
+/**
+ * 617. 合并二叉树
+ * @param {TreeNode} t1
+ * @param {TreeNode} t2
+ * @return {TreeNode}
+ */
+var mergeTrees = function(t1, t2) {
+  if (!t1 || !t2) return t1 ? t1 : t2
+  t1.val = t1.val + t2.val
+  t1.left = mergeTrees(t1.left, t2.left)
+  t1.right = mergeTrees(t1.right, t2.right)
+  return t1
+}
