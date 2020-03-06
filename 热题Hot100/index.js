edd4984988cf36bd70723f7925403501068e458b
滕怡君 <1057120672@qq.com>
2020-03-06 15:54:01 +0800

    762. 二进制表示中质数个计算置位（位运算）

--- 热题Hot100/index.js
+++ 热题Hot100/index.js
@@ -1531,3 +1531,25 @@ var sortByBits = function(arr) {
   arr.sort(compare)
   return arr
 }
+/**
+ * 762. 二进制表示中质数个计算置位（位运算）
+ * @param {number} L
+ * @param {number} R
+ * @return {number}
+ */
+var countPrimeSetBits = function(L, R) {
+  const prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+  let res = 0
+  function countOne(n) {
+    let count = 0
+    for (let i = 32; i > 0; i--) {
+      if ((n & 1) === 1) count++
+      n >>= 1
+    }
+    return count
+  }
+  for (let i = L; i <= R; i++) {
+    if (prime.indexOf(countOne(i)) !== -1) res++
+  }
+  return res
+}
