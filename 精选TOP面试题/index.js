92775cff538de5cea9443ad92e13b02574fb2fed
滕怡君 <1057120672@qq.com>
2020-03-10 14:26:42 +0800

    139. 单词拆分（动态规划）

--- 精选TOP面试题/index.js
+++ 精选TOP面试题/index.js
@@ -24,3 +24,21 @@ var wordBreak = function(s, wordDict) {
   dp(0, 0, '')
   return res
 }
+/**
+ * 139. 单词拆分（动态规划）
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function(s, wordDict) {
+  const len = s.length
+  const dp = new Array(len + 1)
+  dp.fill(false)
+  dp[0] = true
+  for (let i = 1; i <= len; i++) {
+    for (let j = 0; j < i; j++) {
+      if (dp[j] && wordDict.indexOf(s.slice(j, i)) !== -1) dp[i] = true
+    }
+  }
+  return dp[len]
+}
