c959d0e4f5ad48e08a3af9f657a026c8c935cc08
滕怡君 <1057120672@qq.com>
2020-03-12 09:04:50 +0800

    21. 合并两个有序链表（空间O(1)）

--- 链表/index.js
+++ 链表/index.js
@@ -646,3 +646,44 @@ var levelOrder = function(root) {
   }
   return res
 }
+/**
+ * 21. 合并两个有序链表（空间O(1)）
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+  if (!l1 || !l2) return l1 ? l1 : l2
+  const dummy = new ListNode(null)
+  dummy.next = l1.val <= l2.val ? l1 : l2
+  let prev = dummy
+  while (l1 && l2) {
+    if (l1.val <= l2.val) {
+      prev.next = l1
+      while (l1.next && l1.next.val < l2.val) {
+        l1 = l1.next
+        prev = prev.next
+      }
+      let temp = l2.next
+      l2.next = l1.next
+      l1.next = l2
+      l2 = temp
+      prev = prev.next.next
+      l1 = l1.next.next
+    } else {
+      prev.next = l2
+      while (l2.next && l2.next.val < l1.val) {
+        l2 = l2.next
+        prev = prev.next
+      }
+      let temp = l1.next
+      l1.next = l2.next
+      l2.next = l1
+      l1 = temp
+      prev = prev.next.next
+      l2 = l2.next.next
+    }
+  }
+  prev.next = l1 ? l1 : l2
+  return dummy.next
+}
