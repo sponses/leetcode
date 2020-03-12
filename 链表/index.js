41c6c17464e4df81860416ec14e953fa3697620c
滕怡君 <1057120672@qq.com>
2020-03-12 09:16:00 +0800

    21. 合并两个有序链表（递归，空间O(1)）

--- 链表/index.js
+++ 链表/index.js
@@ -687,3 +687,19 @@ var mergeTwoLists = function(l1, l2) {
   prev.next = l1 ? l1 : l2
   return dummy.next
 }
+/**
+ * 21. 合并两个有序链表（递归，空间O(1)）
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+  if (!l1 || !l2) return l1 ? l1 : l2
+  if (l1.val <= l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2)
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next)
+    l1 = l2
+  }
+  return l1
+}
