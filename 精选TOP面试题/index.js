cda8bdc45c6f12f1ecf05da3da207d2a50d6375b
滕怡君 <1057120672@qq.com>
2020-03-16 10:06:34 +0800

    108. 将有序数组转换为二叉搜索树

--- 精选TOP面试题/index.js
+++ 精选TOP面试题/index.js
@@ -983,3 +983,16 @@ var calculate = function(s) {
   }
   return ans
 }
+/**
+ * 108. 将有序数组转换为二叉搜索树
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function(nums) {
+  if (nums.length === 0) return null
+  let mid = Math.floor(nums.length / 2)
+  let root = new TreeNode(nums[mid])
+  root.left = sortedArrayToBST(nums.slice(0, mid))
+  root.right = sortedArrayToBST(nums.slice(mid + 1))
+  return root
+}
