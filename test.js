942f3a5fa0a2a54dd46c08df44c6ac9bc5e29346
滕怡君 <1057120672@qq.com>
2020-03-16 17:18:35 +0800

    14. 最长公共前缀

--- test.js
+++ test.js
@@ -1,22 +1,21 @@
-var numDecodings = function(s) {
-  if (!s.length) return 0
-  if (s[0] == 0) return 0
-  let prev1 = 1,
-    prev2 = 1
-  for (let i = 1, len = s.length; i < len; i++) {
-    let temp
-    if (s[i] == 0) {
-      temp = s[i - 1] == 0 || s[i - 1] > 2 ? 0 : prev1
-    } else if (s[i - 1] == 0) {
-      temp = prev2
-    } else if (+(s[i - 1] + s[i]) > 26) {
-      temp = prev1
-    } else {
-      temp = prev1 + prev2
+var longestCommonPrefix = function(strs) {
+  let ans = ''
+  if (strs.length === 0) return ans
+  if (strs.length === 1) return strs[0]
+  const len = strs.length,
+    firstL = strs[0].length
+  for (let i = 0; i < firstL; i++) {
+    let temp = strs[0].slice(0, i + 1)
+    let j
+    for (j = 1; j < len; j++) {
+      if (temp !== strs[j].slice(0, i + 1)) break
     }
-    prev1 = prev2
-    prev2 = temp
+    if (j === len - 1) {
+      ans = temp
+    } else {
+      break
+    }
   }
-  return prev2
+  return ans
 }
-numDecodings('178')
+longestCommonPrefix(['flower', 'flow', 'flight'])
--- 精选TOP面试题/index.js
+++ 精选TOP面试题/index.js
@@ -1116,3 +1116,27 @@ var countPrimes = function(n) {
   for (let i = 2; i <= n; i++) if (prims[i]) ans++
   return ans
 }
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  let ans = ''
+  if (strs.length === 0) return ans
+  if (strs.length === 1) return strs[0]
+  const len = strs.length,
+    firstL = strs[0].length
+  for (let i = 0; i < firstL; i++) {
+    let temp = strs[0].slice(0, i + 1)
+    let j
+    for (j = 1; j < len; j++) {
+      if (temp !== strs[j].slice(0, i + 1)) break
+    }
+    if (j === len) {
+      ans = temp
+    } else {
+      break
+    }
+  }
+  return ans
+}
