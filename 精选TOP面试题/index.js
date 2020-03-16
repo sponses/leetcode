05aae4b700a6571b2f1f97d4e1b096469961045a
滕怡君 <1057120672@qq.com>
2020-03-16 09:59:08 +0800

    227. 基本计算器 II（栈）

--- 精选TOP面试题/index.js
+++ 精选TOP面试题/index.js
@@ -943,3 +943,43 @@ var maxPathSum = function(root) {
   getMax(root)
   return res
 }
+/**
+ * 227. 基本计算器 II（栈）
+ * @param {string} s
+ * @return {number}
+ */
+var calculate = function(s) {
+  let stack = []
+  for (let i = 0, len = s.length; i < len; i++) {
+    if (s[i] === ' ') continue
+    let cur = ''
+    if (s[i] === '+' || s[i] === '-') {
+      cur = s[i]
+    } else if (s[i] === '*' || s[i] === '/') {
+      let symb = s[i]
+      let prev = stack.pop()
+      let next = ''
+      while (s[i + 1] === ' ' && i < s.length) i++
+      while (!Number.isNaN(+s[i + 1]) && s[i + 1] !== ' ' && i < s.length) {
+        next += s[++i]
+      }
+      cur = symb === '*' ? prev * +next : Math.floor(prev / +next)
+    } else {
+      cur += s[i]
+      while (!Number.isNaN(+s[i + 1]) && s[i + 1] !== ' ' && i < s.length) {
+        cur += s[++i]
+      }
+      cur = +cur
+    }
+    stack.push(cur)
+  }
+  let ans = stack[0]
+  for (let i = 1, len = stack.length; i < len; i++) {
+    if (stack[i] === '+') {
+      ans += stack[++i]
+    } else {
+      ans -= stack[++i]
+    }
+  }
+  return ans
+}
