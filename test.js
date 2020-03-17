7f17e746e03fc5c1f1e24d14a4e1342aaa7be30d
滕怡君 <1057120672@qq.com>
2020-03-17 17:43:34 +0800

    44. 通配符匹配（动态规划）

--- test.js
+++ test.js
@@ -1,21 +1,33 @@
-var numDecodings = function(s) {
-  if (s[0] === 0) return 0
-  let prev1 = 1,
-    prev2 = 1
-  const len = s.length
-  for (let i = 1; i < len; i++) {
-    let temp = 0
-    if (s[i] === 0 && (s[i - 1] === 0 || s[i - 1] > 2)) return 0
-    if (s[i] === 0) {
-      temp = prev1
-    } else if (s[i - 1] === 0 || +(s[i - 1] + s[i]) > 26) {
-      temp = prev2
-    } else {
-      temp = prev1 + prev2
-    }
-    prev1 = prev2
-    prev2 = temp
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function(s, p) {
+  const lenS = s.length,
+    lenP = p.length
+  const dp = new Array(lenS + 1)
+  for (let i = 0; i <= lenS; i++) {
+    dp[i] = new Array(lenP + 1)
+    dp[i].fill(false)
   }
-  return prev2
+  dp[lenS][lenP] = true
+  for (let i = lenP - 1; i >= 0; i--) {
+    dp[lenS][i] = dp[lenS][i] && p[i] === '*'
+  }
+  for (let i = lenS - 1; i >= 0; i--) {
+    for (let j = lenP - 1; j >= 0; j--) {
+      const charS = s[i],
+        charP = p[j]
+      if (charS === charP || charP === '?') {
+        dp[i][j] = dp[i + 1][j + 1]
+      } else if (charP === '*') {
+        for (let n = 0; n <= lenS - i; n++) {
+          dp[i][j] = dp[i][j] || dp[i + n][j + 1]
+        }
+      }
+    }
+  }
+  return dp[0][0]
 }
-numDecodings('2260')
+isMatch('', '*')
--- 精选TOP面试题/index.js
+++ 精选TOP面试题/index.js
@@ -1306,3 +1306,36 @@ var numDecodings = function(s) {
   }
   return prev2
 }
+/**
+ * 44. 通配符匹配（动态规划）
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function(s, p) {
+  const lenS = s.length,
+    lenP = p.length
+  const dp = new Array(lenS + 1)
+  for (let i = 0; i <= lenS; i++) {
+    dp[i] = new Array(lenP + 1)
+    dp[i].fill(false)
+  }
+  dp[lenS][lenP] = true
+  for (let i = lenP - 1; i >= 0; i--) {
+    dp[lenS][i] = dp[lenS][i + 1] && p[i] === '*'
+  }
+  for (let i = lenS - 1; i >= 0; i--) {
+    for (let j = lenP - 1; j >= 0; j--) {
+      const charS = s[i],
+        charP = p[j]
+      if (charS === charP || charP === '?') {
+        dp[i][j] = dp[i + 1][j + 1]
+      } else if (charP === '*') {
+        for (let n = 0; n <= lenS - i; n++) {
+          dp[i][j] = dp[i][j] || dp[i + n][j + 1]
+        }
+      }
+    }
+  }
+  return dp[0][0]
+}
