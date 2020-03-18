bb6aca3654488bcb5fe88f9c85248c5e58938134
滕怡君 <1057120672@qq.com>
2020-03-18 15:59:58 +0800

    面试题53 - II. 0～n-1中缺失的数字

--- test.js
+++ test.js
@@ -1,26 +1,11 @@
-var isNumber = function(s) {
-  if (s.length === 0) return false
-  let len = s.length
-  let i = 0
-  while (s[len - 1] === ' ' && len > 0) len--
-  while (s[i] === ' ' && i < len) i++
-  if (s[i] === '-' || (s[i] === '+' && i < len)) i++
-  if (i === len) return false
-  let e = true,
-    point = true
-  for (let n = i; n < len; n++) {
-    const cur = s[n]
-    if ((n === i || n === len - 1) && cur === 'e') return false
-    if ((cur === '+' || cur === '-') && s[n - 1] !== 'e') return false
-    if (cur === 'e' && e) {
-      e = false
-      point = false
-    } else if (cur === '.' && point) {
-      point = false
-    } else if (Number.isNaN(+cur) || cur === ' ') {
-      return false
-    }
+var missingNumber = function(nums) {
+  let l = 0,
+    r = nums.length - 1
+  while (l <= r) {
+    let mid = (l + r) >> 1
+    if (nums[mid] == mid) l = mid + 1
+    r = mid - 1
   }
-  return true
+  return l
 }
-isNumber('   1e-2323')
+missingNumber([0, 1])
--- 剑指offer/index.js
+++ 剑指offer/index.js
@@ -142,3 +142,21 @@ var reverseWords = function(s) {
   }
   return ans.slice(0, ans.length - 1)
 }
+/**
+ * 面试题53 - II. 0～n-1中缺失的数字
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+  let l = 0,
+    r = nums.length - 1
+  while (l <= r) {
+    let mid = (l + r) >> 1
+    if (nums[mid] === mid) {
+      l = mid + 1
+    } else {
+      r = mid - 1
+    }
+  }
+  return l
+}
