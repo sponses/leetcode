9dbf50c361cad7876d648a4d77cad551e42dc3a2
滕怡君 <1057120672@qq.com>
2020-03-24 08:57:25 +0800

    面试题45. 把数组排成最小的数

--- 剑指offer/index.js
+++ 剑指offer/index.js
@@ -976,3 +976,13 @@ var singleNumber = function(nums) {
   }
   return ans
 }
+/**
+ * 面试题45. 把数组排成最小的数
+ * @param {number[]} nums
+ * @return {string}
+ */
+var minNumber = function(nums) {
+  nums = nums.map(item => item + '')
+  nums.sort((a, b) => a + b - (b + a))
+  return nums.join('')
+}
