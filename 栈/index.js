030e805faac206d12dbc00a974aec0bd352d32c7
滕怡君 <1057120672@qq.com>
2020-05-13 11:24:49 +0800

    155. 最小栈

--- 栈/index.js
+++ 栈/index.js
@@ -146,3 +146,52 @@ var minRemoveToMakeValid = function (s) {
   }
   return [...ans].reverse().join('')
 }
+/**
+ * 155. 最小栈
+ * initialize your data structure here.
+ */
+var MinStack = function () {
+  this.stack = []
+  this.minStack = []
+}
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function (x) {
+  this.stack.push(x)
+  if (!this.minStack.length || this.minStack[this.minStack.length - 1] >= x)
+    this.minStack.push(x)
+}
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  const temp = this.stack.pop()
+  if (this.minStack[this.minStack.length - 1] === temp) this.minStack.pop()
+}
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1]
+}
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  return this.minStack[this.minStack.length - 1]
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
