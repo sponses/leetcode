79f195bae45ec8ffddbe5ad74f93b94b469f7fa8
滕怡君 <1057120672@qq.com>
2020-05-19 09:15:40 +0800

    189. 旋转数组

--- 数组/index.js
+++ 数组/index.js
@@ -260,3 +260,22 @@ var generateMatrix = function (n) {
   }
   return ans
 }
+/**
+ * 189. 旋转数组
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  k %= nums.length
+  function reverse(l, r) {
+    while (l < r) {
+      ;[nums[l], nums[r]] = [nums[r], nums[l]]
+      l++
+      r--
+    }
+  }
+  reverse(0, nums.length - 1)
+  reverse(0, k - 1)
+  reverse(k, nums.length - 1)
+}
